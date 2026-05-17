c19c8899c70f0a2d66663606815b75f83ab9938c
itu-itis24-delioglu22 <delioglu22@itu.edu.tr>
2026-05-17 20:16:53 +0300

    feat: add dynamic slice generation, DOTween spin logic, and NormalWheel data

--- Assets/Scripts/WheelController.cs
+++ Assets/Scripts/WheelController.cs
@@ -1,11 +1,21 @@
 using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI; 
+using DG.Tweening;
+using TMPro;
 
 public class WheelController : MonoBehaviour
 {
     [SerializeField] private Button spinButton;
     [SerializeField] private RectTransform wheelBase;
+    [Header("Spin Settings")]
+    [SerializeField] private float spinDuration = 3f; 
+    [SerializeField] private int spinLoops = 5;
+    private bool isSpinning = false;
+    [Header("Data")]
+    public WheelData activeWheelData;
+    public GameObject slicePrefab;
+
 
     private void OnValidate()
     {
@@ -25,10 +35,67 @@ public class WheelController : MonoBehaviour
         if (spinButton != null)
         spinButton.onClick.AddListener(OnSpinButtonClicked);
     }
+    private void Start()
+    {
+        GenerateWheel();
+    }
+
+    private void GenerateWheel()
+    {
+        foreach (Transform child in wheelBase)
+        {
+            Destroy(child.gameObject);
+        }
+        int totalSlices = activeWheelData.slices.Count;
+        float anglePerSlice = 360f / totalSlices;
+
+        for(int i = 0; i < totalSlices; i++)
+        {
+            GameObject newSlice = Instantiate(slicePrefab, wheelBase);
+            float angle = -anglePerSlice * i;
+            newSlice.transform.localRotation = Quaternion.Euler(0, 0, angle);
+            float radius = 145f;    
+
+            float posAngle = anglePerSlice * i;
+            float x = Mathf.Sin(posAngle * Mathf.Deg2Rad) * radius;
+            float y = Mathf.Cos(posAngle * Mathf.Deg2Rad) * radius;
+            newSlice.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y);
+
+            SliceData data = activeWheelData.slices[i];
+            Image iconImage = newSlice.GetComponent<Image>();
+            if (iconImage != null && data.icon != null)
+                iconImage.sprite = data.icon;
+            TextMeshProUGUI amountText = newSlice.GetComponentInChildren<TextMeshProUGUI>();
+            if (amountText != null)
+            {
+                if (data.amount == 0)
+                    amountText.text = "";
+                else
+                
+                    amountText.text = "x" + data.amount.ToString();
+            }
+        }
+    }
 
     private void OnSpinButtonClicked()
     {
-        Debug.Log("Spinnnnninngggg!!!");
-    }
+        if(isSpinning) return;
 
+        isSpinning = true;
+        spinButton.interactable = false;
+
+        int randomSliceIndex = Random.Range(0, activeWheelData.slices.Count);
+        float spinAngle = 360f / 8f;
+
+        float targetAngle = -spinAngle * randomSliceIndex;
+        float totalRotation = targetAngle - (spinLoops * 360f);
+        
+        wheelBase.DORotate(new Vector3(0, 0, totalRotation), spinDuration, RotateMode.FastBeyond360)
+        .SetEase(Ease.OutCirc).OnComplete(() =>
+        {
+                isSpinning = false;
+                spinButton.interactable = true;
+                Debug.Log($"Çark durdu! Kazanan dilim (0-7): {randomSliceIndex}");
+        });
+    }
 }
